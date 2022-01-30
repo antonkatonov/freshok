e4afa2ce59abc1bc4227b68d4f4cdc40506ab610
antonkatonov <antonkatonov@gmail.com>
2022-01-30 13:35:19 +0200

    header_full

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,19 @@
-$(function name(params) {
-  
-}) ;
+$(function (){
+
+  $('.slider').slick({
+    prevArrow: '<button type="button" class="slick-prev"><img src="images/icons/icon-left-arrow.svg" alt=""></button>',
+    nextArrow: '<button type="button" class="slick-next"><img src="images/icons/icons-right-arrow.svg" alt=""></button>',
+  });
+
+  $('.menu').on('click', function(){
+    $('.menu').toggleClass('menu--active');
+    $('.menu__items').fadeToggle();
+ });
+
+
+  $('.menu').on('click', function(){
+   $('.menu__wrap').removeClass('menu__wrap--active');
+ });
+
+
+});
