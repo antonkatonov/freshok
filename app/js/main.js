38aef9ece9a79f5f02d55c0a7074ab7b3bf77a3a
antonkatonov <antonkatonov@gmail.com>
2022-02-20 13:33:16 +0200

    pochti gotov adaptiv

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,24 @@
 $(function (){
 
+  $('.cart-box__slider-link').on('click', function(){
+    $('.popap').fadeIn();
+    $('body').addClass('lock');
+  });
+
+  $(document).mouseup(function (e) { 
+	var popup = $('.popap');
+	if (e.target!=popup[0]&&popup.has(e.target).length === 0){
+    $('.popap').hide();
+		$('body').removeClass('lock');
+	}
+ });
+
+   $('.popap__close-btn').on('click', function(){
+     $('.popap').hide();
+     $('body').removeClass('lock');
+  });
+
+
   $('.content-filter__button').on('click', function(){
     $('.content-filter__button').removeClass('content-filter__button--active');
     $(this).addClass('content-filter__button--active');
@@ -52,6 +71,14 @@ $(function (){
   fade           : true,
   prevArrow: $('.product-top__slider-arrow--prew'),
   nextArrow: $('.product-top__slider-arrow--next'),
+  });
+
+  $('.related-products__list').slick({
+  infinite: true,
+  slidesToShow: 4,
+  slidesToScroll: 1,
+  prevArrow: $('.related-products__arrow--prew'),
+  nextArrow: $('.related-products__arrow--next'),
   });
 
 
@@ -168,6 +195,7 @@ $(function (){
     ratedFill : "#FFB800",
     normalFill: "#f6f6f6",
     readOnly  : true,
+    spacing   : "6px",
     rating    : 4,
     starSvg: '<svg version="1.1" xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16"><path d="M0.0229654 6.16432C0.0780897 5.9946 0.224746 5.87091 0.401307 5.84529L5.36138 5.12451L7.57965 0.629933C7.65859 0.469933 7.82156 0.368652 7.99996 0.368652C8.1784 0.368652 8.34134 0.469933 8.42031 0.629933L10.6387 5.12451L15.5987 5.84529C15.7752 5.87091 15.9219 5.9946 15.977 6.16429C16.0322 6.334 15.9862 6.52028 15.8584 6.64481L12.2694 10.1434L13.1165 15.0834C13.1467 15.2593 13.0744 15.437 12.9301 15.5419C12.8484 15.6012 12.7517 15.6314 12.6545 15.6314C12.5799 15.6314 12.505 15.6136 12.4364 15.5776L8 13.2451L3.56373 15.5775C3.40576 15.6606 3.21442 15.6467 3.07008 15.5419C2.92574 15.437 2.85339 15.2593 2.88355 15.0834L3.73095 10.1434L0.141559 6.64478C0.0138092 6.52028 -0.0322227 6.334 0.0229654 6.16432Z"></path></svg>',
   });
